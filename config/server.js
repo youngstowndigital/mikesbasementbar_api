c0194027d388f12cda4b54bca6cfa74f8352d714
Mike Wittenauer <mikewittenauer@me.com>
2021-03-31 12:55:08 -0400

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2a7fae581603340dd167c0dc30f2eba8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e0f2684e-c2a4-4aff-83bd-8192bdfb6c26'
+};
